1f06ef12c838c85816c8787a3520e91f10192665
Ian Flint <ianflint@yahoo-inc.com>
2015-06-18 13:48:09 -0700

    Working on option processing

--- src/main/java/com/yahoo/egads/TrainForecastingModel.java
+++ src/main/java/com/yahoo/egads/TrainForecastingModel.java
@@ -1,27 +1,31 @@
 package com.yahoo.egads;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.HashMap;
 
 import com.yahoo.egads.data.FileModelStore;
+import com.yahoo.egads.data.Model;
+import com.yahoo.egads.data.ModelFactory;
 import com.yahoo.egads.data.ModelStore;
 import com.yahoo.egads.data.TimeSeries;
-import com.yahoo.egads.models.tsmm.OlympicModel;
 import com.yahoo.egads.models.tsmm.StreamingOlympicModel;
 
+import gnu.getopt.*;
+
 public class TrainForecastingModel {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		HashMap<Integer,String> options = processOptions(args);
 		HashMap<String, TimeSeries.DataSequence> inputs = new HashMap<String, TimeSeries.DataSequence>();
 		Scanner sc = new Scanner(System.in);
-		ModelStore m = new FileModelStore ("models");
+		ModelStore ms = new FileModelStore ("models");
 		Properties osProps = new Properties();
-		osProps.setProperty("TIME_SHIFTS", "0,1");
-		osProps.setProperty("BASE_WINDOWS", "24,168");
-		osProps.setProperty("NUM_WEEKS", "6");
-		osProps.setProperty("NUM_TO_DROP", "1");
+		osProps.load (new FileInputStream(options.get('p')));
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 			String[] fields = line.split(",");
@@ -43,16 +47,47 @@ public class TrainForecastingModel {
 			seq.add(new TimeSeries.Entry(timestamp, measured));				
 		}
 		sc.close();
+		ModelFactory mf = new ModelFactory(osProps);
 		for (String series : inputs.keySet()) {
 			TimeSeries.DataSequence seq = inputs.get(series);
+			Model m = mf.getModel(options.get('m'));
 			StreamingOlympicModel o = new StreamingOlympicModel(osProps);
 			o.train(seq);
 //			System.out.println (series + ":");
 //			for (TimeSeries.Entry e : seq) {
 //				System.out.println(e.time + ": " + e.value);
 //			}
-			m.storeModel(series, o);
+			ms.storeModel(series, o);
 		}
+	}
+	
+	public static void usage() {
+		System.out.println ("Usage: TrainForecastingModel [-m <modeltype>] [-p <properties file>] [-h]");
+		System.out.println ("  Modeltypes:");
+		System.out.println ("    sos: Streaming Olympic Scoring");
+		System.out.println ("  Default preperties file is config.ini");
+		System.exit(0);
+	}
+	
+	public static HashMap<Integer, String> processOptions (String[] args) {
+		HashMap<Integer, String> result = new HashMap<Integer, String>();
+		// defaults
+		result.put(new Integer('m'),  "sos");
+		result.put(new Integer('p'), "config.ini");
+
+		Getopt g = new Getopt("TrainForecastingModel", args, "m:p:h");
+		int c;
+		while ((c = g.getopt()) != -1) {
+			switch (c) {
+			case 'm':
+			case 'p':
+				result.put(c, g.getOptarg());
+				break;
+			case 'h':
+				usage();
+			}
+		}
+		return result;
 	}
 
 }
